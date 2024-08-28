fe788a6cef479a9c65eebbf24fe7703262f8a211
Felipe Bronzo <felipebronzo@gmail.com>
2024-08-27 21:48:44 -0300

    Segundo

--- Main.java
+++ Main.java
@@ -5,42 +5,66 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
-import java.text.SimpleDateFormat;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-         SimpleDateFormat formatodedata = new SimpleDateFormat("dd/MM/yyyy");
+        SimpleDateFormat formatodedata = new SimpleDateFormat("dd/MM/yyyy");
 
-         FileOutputStream arq; 
-         DataOutputStream dos;
-         byte[] ba; 
+        FileOutputStream arq; 
+        DataOutputStream dos;
+        byte[] ba; 
 
-        
-        
-         try {
-            Date date;
-            date = formatodedata.parse("15/07/2016");    
+        try {
+            // Registro 1
+            Date date = formatodedata.parse("15/07/2016");    
             ArrayList<String> companies = new ArrayList<>();
             companies.add("Netflix");
             companies.add("Lionsgate");
             companies.add("Paramount");
-            Serie serie = new Serie(99, "Stranger things", "En", date, companies);
 
-            arq = new FileOutputStream("dados/series.db");
+            Serie serie = new Serie(1, "Stranger Things", "En", date, companies);
+
+            arq = new FileOutputStream("dados/series.db"); 
             dos = new DataOutputStream(arq);
             ba = serie.toByteArray();
-            arq.write(ba.length);
-            arq.write(ba);
+            dos.writeBoolean(true);
+            dos.writeInt(ba.length);
+            dos.write(ba);
             System.out.println(serie);
-            } catch (ParseException  | IOException e) {
-               System.out.println("Error");
-            }  // Exemplo de data
-            
-                
-            
-           
-    
-    
-}
+
+            // Registro 2
+            Date date1 = formatodedata.parse("01/02/2013");    
+            ArrayList<String> companies1 = new ArrayList<>();
+            companies1.add("Warner");
+            companies1.add("Amazon");
+
+            Serie serie1 = new Serie(2, "Breaking Bad", "PT", date1, companies1);
+
+            ba = serie1.toByteArray();
+            dos.writeBoolean(true);
+            dos.writeInt(ba.length);
+            dos.write(ba);
+            System.out.println(serie1);
+
+            // Registro 3
+            Date date2 = formatodedata.parse("23/05/2019");    
+            ArrayList<String> companies2 = new ArrayList<>();
+            companies2.add("HBO");
+            companies2.add("Sky Atlantic");
+
+            Serie serie2 = new Serie(3, "Chernobyl", "En", date2, companies2);
+
+            ba = serie2.toByteArray();
+            dos.writeBoolean(true);
+            dos.writeInt(ba.length);
+            dos.write(ba);
+            System.out.println(serie2);
+
+            dos.close(); // Fecha o DataOutputStream
+            arq.close(); // Fecha o FileOutputStream
+        } catch (ParseException | IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
 }
